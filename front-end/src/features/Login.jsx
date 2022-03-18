9df962c42be70fc58056d88313c18748606369c6
Kevin Macquat <macquat.kevin@gmail.com>
2022-03-18 16:52:32 +0100

    add login field

--- front-end/src/features/Login.jsx
+++ front-end/src/features/Login.jsx
@@ -1,28 +1,28 @@
 import jwtDecode from "jwt-decode";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useDispatch } from 'react-redux';
 
 import { addUser, addToken } from "./userSlice";
 
 function Login() {
-  const [token, setToken] = useState('');
-  // const [email, setEmail] = useState('');
-  // const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    (async function() {
-      const url = 'http://ecf.local/api/login';
+  async function login(e) {
+    e.preventDefault();
+
+    const url = 'http://ecf.local/api/login';
+
+      if(email === "" || password === "") {
+        return
+      }
 
       const postData = {
-        "email": "admin@admin.com",
-        "password": "admin"
+        "email": email,
+        "password": password
       }
-      // const postData = {
-      //   "email": "user@user.com",
-      //   "password": "user"
-      // }
 
       const response = await fetch(url, {
         method: "POST",
@@ -32,42 +32,38 @@ function Login() {
       });
 
       const tokenData = await response.json();
-      console.log('login component');
-      console.log(tokenData);
-      console.log(tokenData.token);
 
-      setToken(tokenData);
       dispatch(addToken(tokenData.token));
-      console.log(token);
 
       const tokenForDecode = await jwtDecode(tokenData.token);
       dispatch(addUser(tokenForDecode));
-      console.log(tokenForDecode);
-
-      const postData2 = {
-        "email": "user@user.com",
-        "password": "user"
-      }
-
-      const response2 = await fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"},
-        body: JSON.stringify(postData2),
-      });
-      const tokenData2 = await response2.json();
-      console.log('user');
-      console.log(tokenData2);
-      const tokenForDecode2 = await jwtDecode(tokenData2.token);
-      console.log(tokenForDecode2);
-
-    }());
-
-  }, []);
+  }
 
   return(
     <>
       <h1>Login</h1>
+      <form>
+        <label>
+          Email:
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button
+          type="submit"
+          onClick={(e) => login(e)}
+        >login</button>
+      </form>
     </>
   )
 }
